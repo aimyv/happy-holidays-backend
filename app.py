fbab85f56c9fb9b86fcb476a1fc5d8f8b120958e
aimyv <av533@bath.ac.uk>
2022-12-01 17:18:01 +0000

    added .env

--- app.py
+++
@@ -1,5 +0,0 @@
-from server import create_app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
--- server/__init__.py
+++ server/__init__.py
@@ -1,3 +1,6 @@
+from .models.models import User
+from .routes.auth import auth
+from .routes.views import views
 from flask import Flask, jsonify, render_template
 from werkzeug import exceptions
 # from flask_sqlalchemy import SQLAlchemy
@@ -8,39 +11,39 @@ from .database.db import db
 
 DB_NAME = 'database.db'
 
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = "helloworld"
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+db.init_app(app)
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-    app.config['SECRET_KEY'] = "helloworld"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
 
-    from .routes.views import views
-    from .routes.auth import auth
+app.register_blueprint(views, url_prefix="/")
+app.register_blueprint(auth, url_prefix="/")
 
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
 
-    from .models.models import User
+with app.app_context():
+    db.create_all()
 
-    with app.app_context():
-        db.create_all()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-    @app.errorhandler(exceptions.NotFound)
-    def handle_404(err):
-        return jsonify({"message": f"Oops... {err}"}), 404
 
-    @app.errorhandler(exceptions.InternalServerError)
-    def handle_500(err):
-        return jsonify({"message": f"It's not you it's us"}), 500
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return jsonify({"message": f"Oops... {err}"}), 404
 
-    return app
+
+@app.errorhandler(exceptions.InternalServerError)
+def handle_500(err):
+    return jsonify({"message": f"It's not you it's us"}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
