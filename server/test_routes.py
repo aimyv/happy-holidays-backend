f81c274121dcde34804606a66c8b318a1341b8b4
aimyv <av533@bath.ac.uk>
2022-12-07 11:41:20 +0000

    tested login and logout

--- server/test_routes.py
+++ server/test_routes.py
@@ -52,6 +52,39 @@ class TestAPICase():
         res = api.post('/register', data=mock_data, headers=mock_headers)
         assert res.status == '400 BAD REQUEST'
 
+    def test_login(self, api):
+        mock_data = json.dumps({
+            "email": "test@test.com",
+            "password": "123"
+        })
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/login', data=mock_data, headers=mock_headers)
+        assert res.status == '201 CREATED'
+        assert res.json['username'] == "test"
+        assert res.json['message'] == "Logged In."
+
+    def test_login_incorrect_email(self, api):
+        mock_data = json.dumps({
+            "email": "test1@test.com",
+            "password": "123"
+        })
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/login', data=mock_data, headers=mock_headers)
+        assert res.status == '404 NOT FOUND'
+
+    def test_login_incorrect_password(self, api):
+        mock_data = json.dumps({
+            "email": "test@test.com",
+            "password": "12"
+        })
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/login', data=mock_data, headers=mock_headers)
+        assert res.status == '400 BAD REQUEST'
+
+    def test_logout(self, api):
+        res = api.get('/logout')
+        assert res.status == '200 OK'
+
     def test_delete_user(self, api):
         res = api.delete('/users/2')
         assert res.status == '204 NO CONTENT'
