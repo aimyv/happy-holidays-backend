04494f3ac47c5949e77f1ad5e899706b7d4a9555
aimyv <av533@bath.ac.uk>
2022-12-07 11:34:04 +0000

    added test register routes

--- server/test_routes.py
+++ server/test_routes.py
@@ -6,3 +6,52 @@ class TestAPICase():
         res = api.get('/')
         assert res.status == '200 OK'
         assert res.json['message'] == 'Hello, from Flask!'
+
+    def test_register(self, api):
+        mock_data = json.dumps({
+            "email": "test@test.com",
+            "username": "test",
+            "password1": "123",
+            "password2": "123"
+        })
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/register', data=mock_data, headers=mock_headers)
+        assert res.status == '201 CREATED'
+        assert res.json['message'] == 'User created!'
+
+    def test_existing_email(self, api):
+        mock_data = json.dumps({
+            "email": "test@test.com",
+            "username": "test",
+            "password1": "123",
+            "password2": "123"
+        })
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/register', data=mock_data, headers=mock_headers)
+        assert res.status == '400 BAD REQUEST'
+
+    def test_existing_username(self, api):
+        mock_data = json.dumps({
+            "email": "test1@test.com",
+            "username": "test",
+            "password1": "123",
+            "password2": "123"
+        })
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/register', data=mock_data, headers=mock_headers)
+        assert res.status == '400 BAD REQUEST'
+
+    def test_mismatched_passwords(self, api):
+        mock_data = json.dumps({
+            "email": "test1@test.com",
+            "username": "test",
+            "password1": "12",
+            "password2": "123"
+        })
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/register', data=mock_data, headers=mock_headers)
+        assert res.status == '400 BAD REQUEST'
+
+    def test_delete_user(self, api):
+        res = api.delete('/users/2')
+        assert res.status == '204 NO CONTENT'
