fe088f99ed4ce236517101a42362ac001c5b7fe9
aimyv <av533@bath.ac.uk>
2022-12-07 12:52:30 +0000

    tested /users/:id/friends endpoint

--- server/test_routes.py
+++ server/test_routes.py
@@ -81,10 +81,53 @@ class TestAPICase():
         res = api.post('/login', data=mock_data, headers=mock_headers)
         assert res.status == '400 BAD REQUEST'
 
+    def test_get_all_users(self, api):
+        res = api.get('/users')
+        assert res.status == '200 OK'
+
+    def test_get_specific_user(self, api):
+        test = api.get('/users/test')
+        assert test.status == '200 OK'
+
+    def test_get_nonexistant_user(self, api):
+        test = api.get('/users/test1')
+        assert test.status == '400 BAD REQUEST'
+        res = api.get('/users/-1')
+        assert test.status == '400 BAD REQUEST'
+
+    def test_get_friends(self, api):
+        test = api.get('/users/test')
+        assert test.status == '200 OK'
+        user_id = test.json["id"]
+        res = api.get(f'/users/{user_id}/friends')
+        assert test.status == '200 OK'
+
+    def test_add_friend(self, api):
+        test = api.get('/users/test')
+        user_id = test.json["id"]
+        mock_data = json.dumps({
+            "email": "test2@test2.com",
+            "username": "test2",
+            "password1": "123",
+            "password2": "123"
+        })
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/register', data=mock_data, headers=mock_headers)
+        mock_data = json.dumps({
+            "friend": "test2",
+        })
+        res = api.post(f'/users/{user_id}/friends',
+                       data=mock_data, headers=mock_headers)
+        assert res.status == '201 CREATED'
+        res = api.get(f'/users/{user_id}/friends')
+        assert len(res.json) == 1
+
     def test_logout(self, api):
         res = api.get('/logout')
         assert res.status == '200 OK'
 
     def test_delete_user(self, api):
-        res = api.delete('/users/2')
+        res = api.delete('/users/test')
+        assert res.status == '204 NO CONTENT'
+        res = api.delete('/users/test2')
         assert res.status == '204 NO CONTENT'
