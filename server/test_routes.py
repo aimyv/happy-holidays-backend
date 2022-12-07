d57845aa9c85e8c46bf2b86e1269df9b66748e9f
aimyv <av533@bath.ac.uk>
2022-12-07 11:15:23 +0000

    renamed test file

--- server/test_routes.py
+++ server/test_routes.py
@@ -3,6 +3,6 @@ import json
 
 class TestAPICase():
     def test_home(self, api):
-        resp = api.get('/')
-        assert resp.status == '200 OK'
+        res = api.get('/')
+        assert res.status == '200 OK'
         assert res.json['message'] == 'Hello, from Flask!'
